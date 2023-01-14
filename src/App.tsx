94f935ef594a2203364a90752e6c9b23439d92a2
Yige <tjzhangyige@gmail.com>
2023-01-13 19:12:43 -0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+      <ExploreTopBooks/>
     </div>
+    
   );
 }
 
---
+++ src/layouts/HomePage/ExploreTopBooks.tsx
@@ -0,0 +1,14 @@
+export const ExploreTopBooks = () => {
+    return (
+        <div className="p-5 mb-4 bg-light header">
+            <div className="container-fluid py-5 text-dark
+            d-flex justify-content-center align-items-center">
+                <div>
+                    <h1 className="display-5 fw-bold">Create your documentation of life</h1>
+                    <p className="col-md-8 fs-4">What do you want to record now?</p>
+                    <a type='button' className="btn main-color btn-lg text-white" href="#">Create an item</a>
+                </div>
+            </div>
+        </div>
+    );
+}
